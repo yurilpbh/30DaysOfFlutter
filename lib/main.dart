53a94d070849429ebac13140d350be79c87f0fc2
yurilpbh <yurilpbh@hotmail.com>
2020-10-08 14:46:40 -0300

    Day_5

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-        title: "App 1", //Title of the App
-        home: Scaffold( //This is the widget that construct the homepage
-          backgroundColor: Colors.grey,
-          appBar: AppBar(
-            title: Text("Day 4"), //Title of our homepage
-            leading: Icon(Icons.home),
-            backgroundColor: Colors.black26,
+      title: "App 2", //Title of the App
+      home: Scaffold( //This is the widget that construct the homepage
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text("Container Widget"), //Title of our homepage
+          leading: Icon(Icons.home),
+          backgroundColor: Colors.black26,
+        ),
+        body: Container(
+          //height: 100,
+          width: 100,
+          margin: EdgeInsets.all(20),
+          alignment: Alignment.center,
+          child: Text("Hey Coders!!!"),
+          decoration: BoxDecoration(
+            border: Border.all(width: 4,color: Colors.black),
+            color: Colors.orangeAccent,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.orange, offset: Offset(6.0,6.0),
+              )
+            ]
           ),
-          body: Center(child: Text("Hello world"),
-          ),
-        )
+          transform: Matrix4.rotationZ(0.1),
+          constraints: BoxConstraints.expand(height: 50.0),
+        ),
+      ),
     );
   }
 }
