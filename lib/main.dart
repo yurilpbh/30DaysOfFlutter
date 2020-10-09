5c8a2871ace4e4cc5bebba8684e5ff1b4f6f3f3e
yurilpbh <yurilpbh@hotmail.com>
2020-10-09 18:05:41 -0300

    Day_7

--- lib/main.dart
+++ lib/main.dart
@@ -8,41 +8,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "App 2", //Title of the App
+      title: "App 3", //Title of the App
       home: Scaffold( //This is the widget that construct the homepage
         backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: Text("Container Widget"), //Title of our homepage
+          title: Text("Day 7"), //Title of our homepage
           leading: Icon(Icons.home),
           backgroundColor: Colors.black26,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              height: 50,
-              width: 100,
-              color: Colors.orange,
-              margin: EdgeInsets.all(10),
-              child: Text("CodeWarriors"),
+          children: [
+            Text(
+                "Hello World!!! Coders",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700,
+                    backgroundColor: Colors.amberAccent,
+                    letterSpacing: 1.0,
+                    wordSpacing: 1.0,
+                    shadows: [
+                      Shadow(
+                        color: Colors.lightBlueAccent,
+                        offset: Offset(2.0,2.0),
+                      )
+                    ]
+                )
             ),
-            Container(
-              height: 50,
-              width: 100,
-              color: Colors.orange,
-              margin: EdgeInsets.all(10),
-              child: Text("Hey"),
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: RichText(
+                text: TextSpan(
+                  text: 'Hello!!!',
+                  style: TextStyle(
+                    color: Colors.amber,
+                  ),
+                  children:  <TextSpan>[
+                    TextSpan(
+                      text: 'Coders',
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                      ),
+                    )
+                  ]
+                )
+             ),
+           ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name: ',
+                  hintText: 'Enter Your name',
+                ),
+              ),
             ),
-            Container(
-              height: 50,
-              width: 100,
-              color: Colors.orange,
-              margin: EdgeInsets.all(10),
-              child: Text("Coders!!!"),
-            )
-          ],
+          ]
         ),
       ),
     );
