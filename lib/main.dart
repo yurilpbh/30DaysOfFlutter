e18ffcf7d5ee116e68890c56b6e523ce0aaa42ed
yurilpbh <yurilpbh@hotmail.com>
2020-10-14 18:57:43 -0300

    Day_12

--- lib/main.dart
+++ lib/main.dart
@@ -9,78 +9,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "App 7", //Title of the App
+      title: "App 8", //Title of the App
       home: Scaffold( //This is the widget that construct the homepage
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey,
         appBar: AppBar(
-          title: Text("Forms"), //Title of our homepage
-          backgroundColor: Colors.black26,
+          title: Text("Day 12"), //Title of our homepage
+          backgroundColor: Colors.black54,
         ),
-        body: Forms()
+        body: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.home,
+                    color: Colors.green,
+                    size: 40,
+                    textDirection: TextDirection.rtl
+                  ),
+                  Text("Home"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.access_alarm,
+                      color: Colors.red,
+                      size: 40,
+                      textDirection: TextDirection.rtl
+                  ),
+                  Text("Alarm"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.delete,
+                      color: Colors.black,
+                      size: 40,
+                      textDirection: TextDirection.rtl
+                  ),
+                  Text("Delete"),
+                ],
+              ),
+            ],
+          ),
+        )
       )
-    );
-  }
-}
-
-class Forms extends StatefulWidget {
-  @override
-  _FormsState createState() => _FormsState();
-}
-class _FormsState extends State<Forms> {
-  final keys = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context){
-    return Form(
-      key: keys,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter yor name",
-              labelText: "Name",
-              icon: Icon(Icons.person),
-            ),
-            validator: (value){
-              if(value.isEmpty){
-                return 'Please fill the details';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Enter your password",
-              labelText: "Password",
-              icon: Icon(Icons.lock),
-            ),
-            validator: (value){
-              if(value.isEmpty){
-                return 'Please fill the details';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: "Github link",
-              labelText: "Github",
-              icon: Icon(Icons.link),
-            ),
-          ),
-          Container(
-            child: RaisedButton(
-              child: Text("Submit"),
-              onPressed: (){
-                if(keys.currentState.validate()){
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text("Validation Going on")));
-                }
-              },
-            ),
-          )
-        ],
-      ),
     );
   }
 }
