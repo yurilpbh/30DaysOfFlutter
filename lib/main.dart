39787c39abe68dddc6966a6b6f025dcb8f985482
yurilpbh <yurilpbh@hotmail.com>
2020-10-16 20:46:35 -0300

    Day_11

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main () {
@@ -9,56 +10,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "App 10", //Title of the App
-      home: Scaffold( //This is the widget that construct the homepage
-        backgroundColor: Colors.grey,
-        appBar: AppBar(
-          title: Text("Day 14"), //Title of our homepage
-          backgroundColor: Colors.black54,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(28.0),
-          child: Center(
-            child: Container(
-              height: 200,
-              width: 300,
-              child: Card(
-                color: Colors.yellow,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: ListTile(
-                        leading: Icon(Icons.person, size: 40, color: Colors.blue,),
-                        title: Text("Yuri Pereira", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black)),
-                        subtitle: Text("COO", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15)),
-                      ),
-                    ),
-                    ButtonBar(
-                      children: [
-                        RaisedButton(
-                          child: FloatingActionButton.extended(onPressed: (){},label: Text("Contact"), icon: Icon(Icons.call)),
-                          color: Colors.blue,
-                          onPressed: (){}
-                        ),
-                        RaisedButton(
-                            child: Text("Link"),
-                            color: Colors.blue,
-                            onPressed: (){}
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
+      title: "App 11", //Title of the App
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("TabBar"),
+            backgroundColor: Colors.black87,
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home), text: "Home"),
+                Tab(icon: Icon(Icons.settings), text: "Settings"),
+                Tab(icon: Icon(Icons.local_grocery_store), text: "Store")
+              ],
             ),
           ),
-        )
+          body: TabBarView(
+            children: [
+              HomePage(),
+              SettingsPage(),
+              StorePage()
+            ],
+          ),
+        ),
       )
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text("Home", style: TextStyle(fontSize: 60))
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          child: Text("Settings", style: TextStyle(fontSize: 60))
+      ),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          child: Text("Store", style: TextStyle(fontSize: 60))
+      ),
     );
   }
 }
