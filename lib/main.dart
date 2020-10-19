a2974aa635989e2955e63f6bb6de8b23330155f5
yurilpbh <yurilpbh@hotmail.com>
2020-10-19 14:42:51 -0300

    Day_18

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,191 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/newcard.dart';
 
-void main(){
-  runApp(myApp(
-    //names: List<String>.generate(50, (a) => "Coders! $a")
-  ));
+void main() {
+  runApp(myApp());
 }
 
-class myApp extends StatelessWidget {
-  // final List<String> names;
-  //
-  // myApp({Key key, this.names}):super(key:key);
+class myApp extends StatefulWidget {
+  @override
+  _myAppState createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+
+  String output = "0", operand = "";
+
+  double num1 = 0, num2 = 0, auxOutput = 0;
+
+  operation(String btnText){
+    if(btnText == "C"){
+        output = "0";
+        auxOutput = 0;
+        num1 = 0;
+        num2 = 0;
+    } else if (btnText == "/" || btnText == "*" || btnText == "+" || btnText == "-"){
+      num1 = double.parse(output);
+      operand = btnText;
+    }else if (operand != ""){
+      num2 = double.parse(btnText);
+      switch (operand) {
+        case "/":
+          auxOutput = num1 / num2;
+          break;
+        case "*":
+          auxOutput = num1 * num2;
+          break;
+        case "+":
+          auxOutput = num1 + num2;
+          break;
+        case "-":
+          auxOutput = num1 - num2;
+          break;
+        default:
+          break;
+      }
+      operand = "";
+    }
+    else{
+      auxOutput = double.parse(btnText);
+    }
+
+    setState((){
+      output = auxOutput.toStringAsFixed(2);
+    });
+  }
+
+  Widget button(String btnText){
+    return Expanded(
+        child: RawMaterialButton(
+          shape: Border.all(color: Colors.black, width: 2),
+          fillColor: Colors.black12,
+          padding: EdgeInsets.all(20.0),
+          child: Text(
+            btnText,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          onPressed: (){
+            operation(btnText);
+          },
+        )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Horizontal List"),
+          title: Text("Calculator"),
+          backgroundColor: Colors.black12,
         ),
         body: Container(
-          height: 150.0,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Container(
-                width: 148.0,
-                color: Colors.yellow,
-                child: Center(
-                  child: ListTile(
-                    leading: Icon(Icons.home),
-                    title: Text("Home"),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    color: Colors.black26
+                  ),
+                  padding: EdgeInsets.all(20.0),
+                  margin: EdgeInsets.only(top: 2.0, bottom: 2.0),
+                  child: Text(
+                    "$output",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600
+                    ),
                   ),
                 ),
               ),
-              Container(
-                width: 148.0,
-                color: Colors.red,
-                child: Center(
-                  child: ListTile(
-                    leading: Icon(Icons.accessible),
-                    title: Text("Acessibility"),
-                  ),
+              Row(
+                children: [
+                button("9"),
+                SizedBox(
+                  width: 2,
                 ),
+                button("8"),
+                SizedBox(
+                  width: 2,
+                ),
+                button("7"),
+                SizedBox(
+                  width: 2,
+                ),
+                button("-"),
+                ],
               ),
-              Container(
-                width: 148.0,
-                color: Colors.green,
-                child: Center(
-                  child: ListTile(
-                    leading: Icon(Icons.cached),
-                    title: Text("Update"),
-                  ),
-                ),
+              SizedBox(
+                height: 2
               ),
-              Container(
-                width: 148.0,
-                color: Colors.blue,
-                child: Center(
-                  child: ListTile(
-                    leading: Icon(Icons.g_translate),
-                    title: Text("Translate"),
+              Row(
+                children: [
+                  button("6"),
+                  SizedBox(
+                    width: 2,
                   ),
-                ),
+                  button("5"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("4"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("+"),
+                ],
+              ),
+              SizedBox(
+                  height: 2
+              ),
+              Row(
+                children: [
+                  button("3"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("2"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("1"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("*"),
+                ],
+              ),
+              SizedBox(
+                  height: 2
+              ),
+              Row(
+                children: [
+                  button("0"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  /*button("."),
+                  SizedBox(
+                    width: 2,
+                  ),*/
+                  button("C"),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  button("/"),
+                ],
               ),
             ],
           ),
-        )
+        ),
       ),
     );
   }
 }
-
-class data {
-  const data({this.title, this.icon});
-  final String title;
-  final IconData icon;
-}
-
-const List<data> datas = const <data>[
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-  const data(title: 'Home', icon: Icons.home),
-];
