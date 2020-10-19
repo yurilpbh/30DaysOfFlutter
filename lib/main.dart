e20f4fd69c1f1e46479f901ce8a39874ba8dc87b
yurilpbh <yurilpbh@hotmail.com>
2020-10-19 10:40:14 -0300

    Day 17 - Long List

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(myApp());
+  runApp(myApp(
+    names: List<String>.generate(50, (a) => "Coders! $a")
+  ));
 }
 
 class myApp extends StatelessWidget {
+  final List<String> names;
+
+  myApp({Key key, this.names}):super(key:key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Basic List"),
+          title: Text("Long List"),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-            ),
-            ListTile(
-              leading: Icon(Icons.accessible),
-              title: Text("Acessibility"),
-            ),
-            ListTile(
-              leading: Icon(Icons.access_alarm),
-              title: Text("Time"),
-            ),
-          ],
+        body: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              title: Text("Hi ${names[index]}"),
+            );
+          },
         ),
       ),
     );
