11e600b70f71cdb0318413c5e29c4825dc1296ad
yurilpbh <yurilpbh@hotmail.com>
2020-10-19 10:33:36 -0300

    Day 17 - ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(MyApp());
+void main(){
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AlertWidget()
-    );
-  }
-}
-
-class AlertWidget extends StatelessWidget {
-  //TextEditingController _textEditingController = new TextEditingController();
+class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child: RaisedButton(
-            child: Text("Basic AlertDialog"),
-            onPressed: () async{
-              //alertDialogWidget(context);
-              //textFieldAlertDialogWidget(context, _textEditingController);
-              //  field fieldAtt = await confirmAlertDialogWidget(context);
-              //  print("User selection $fieldAtt");
-               final selectOpt option = await selectionOptionAlertDialogWidget(context);
-               print (option);
-            },
-            color: Colors.blue,
-          ),
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic List"),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            ListTile(
+              leading: Icon(Icons.accessible),
+              title: Text("Acessibility"),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_alarm),
+              title: Text("Time"),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-alertDialogWidget(BuildContext context){
-  showDialog(context: context,
-    builder: (BuildContext context){
-      return AlertDialog(
-        title: Text("Basic"),
-        content: Text("This is for notification purpose!!!"),
-        actions: [
-          RaisedButton(
-            child: Text("OK"),
-            color: Colors.blue,
-            onPressed: (){
-              Navigator.of(context).pop();
-            },
-          )
-        ],
-      );
-    }
-  );
-}
-
-textFieldAlertDialogWidget(BuildContext context, TextEditingController textEditingController){
-  showDialog(context: context,
-      builder: (BuildContext context){
-        return AlertDialog(
-          title: Text("TextField"),
-          content: TextField(
-            decoration: InputDecoration(
-              hintText: "Enter anything that you want"
-            ),
-            controller: textEditingController,
-          ),
-          actions: [
-            RaisedButton(
-              child: Text("OK"),
-              color: Colors.blue,
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      }
-  );
-}
-
-enum field {Cancel, Accept}
-Future<field> confirmAlertDialogWidget(BuildContext context) async{
-
-  return showDialog<field>(context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context){
-      return AlertDialog(
-        title: Text("Confirm"),
-        content: Text("This is for notification purpose"),
-        actions: [
-          RaisedButton(
-            child: Text("Cancel"),
-            color: Colors.blue,
-            onPressed: (){
-              Navigator.of(context).pop(field.Cancel);
-            },
-          ),
-          RaisedButton(
-            child: Text("Accept"),
-            color: Colors.blue,
-            onPressed: (){
-              Navigator.of(context).pop(field.Accept);
-            },
-          )
-        ],
-      );
-    }
-  );
-}
-
-enum selectOpt {C,Java,Python,PHP}
-Future<selectOpt> selectionOptionAlertDialogWidget(BuildContext context) async{
-
-  return showDialog<selectOpt>(
-    context: context,
-    barrierDismissible: true,
-    builder: (BuildContext context){
-      return SimpleDialog(
-        title: Text("Choose any one"),
-        children: [
-          SimpleDialogOption(
-            onPressed: (){
-              Navigator.pop(context, selectOpt.C);
-            },
-            child: Text("C"),
-          ),
-          SimpleDialogOption(
-            onPressed: (){
-              Navigator.pop(context, selectOpt.Java);
-            },
-            child: Text("Java"),
-          ),
-          SimpleDialogOption(
-            onPressed: (){
-              Navigator.pop(context, selectOpt.Python);
-            },
-            child: Text("Python"),
-          ),
-          SimpleDialogOption(
-            onPressed: (){
-              Navigator.pop(context, selectOpt.PHP);
-            },
-            child: Text("PHP"),
-          )
-        ],
-      );
-    }
-  );
 }
