a14d5738b8a1a087d30321869ac398f723299909
yurilpbh <yurilpbh@hotmail.com>
2020-10-20 16:25:23 -0300

    Day_19 Toast

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
-
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  List<String> images = [
-    "https://images.unsplash.com/photo-1600856268703-63e89b354c58?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
-    "https://images.unsplash.com/photo-1502781252888-9143ba7f074e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-    "https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-    "https://images.unsplash.com/photo-1600856268703-63e89b354c58?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
-    "https://images.unsplash.com/photo-1502781252888-9143ba7f074e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-    "https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-  ];
+
+  void showToast(){
+    Fluttertoast.showToast(msg: "This is one notification",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: Colors.blueAccent,
+      textColor: Colors.black,
+      fontSize: 20
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,53 +26,16 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text("GridView"),
           ),
-          body: GridView.extent(
-            primary: false,
-            padding: EdgeInsets.all(8),
-            maxCrossAxisExtent: 250,
-            crossAxisSpacing: 8,
-            children: [
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
+          body: Center(
+            child: Container(
+              child: RaisedButton(
+                child: Text("This is toast"),
+                onPressed: (){
+                  showToast();
+                },
+                color: Colors.blue,
               ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-              Container(
-                  child: Image.network("https://images.unsplash.com/photo-1589641896739-8ba0d81b383a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-                      width: 200,
-                      height: 200)
-              ),
-            ],
+            ),
           )
       ),
     );
